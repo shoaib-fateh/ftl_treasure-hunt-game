bda890d425ec515f529fb55fdad9544a053233a6
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-19 19:41:37 +0430

    playing around and add colors to look better 🌈😎

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import random
+from colorama import Fore, Style, init
+
+init()
 
 rows = int(input("Enter the number of rows for the game board: "))
 cols = int(input("Enter the number of columns for the game board: "))
 
+
 board = []
-
-
-
 for r in range(rows):
     row = []
-
     for c in range(cols):
         row.append("*")
-
     board.append(row)
+
+
+
 
 treasure_row = random.randint(0, rows - 1)
 treasure_col = random.randint(0, cols - 1)
 
+
 board[treasure_row][treasure_col] = "T"
+
+
+print("\n" + Fore.GREEN + Style.BRIGHT + "Game Board:" + Style.RESET_ALL)
+
 
 for row in board:
     print(" ".join(row))
-
+print("\n" + "-" * 20 + "\n")
 
 
 while True:
     try:
-        guess_row = int(input(f"Choose a row to hide the treasure (0 to {rows - 1}): "))
-        guess_col = int(input(f"Choose a column to hide the treasure {cols - 1}): "))
+
+        guess_row = int(input(f"{Fore.YELLOW}Choose a row to hide the treasure (0 to {rows - 1}): {Style.RESET_ALL}"))
+        guess_col = int(input(f"{Fore.YELLOW}Choose a column to hide the treasure (0 to {cols - 1}): {Style.RESET_ALL}"))
 
         if guess_row == treasure_row and guess_col == treasure_col:
-            print("Great, You win!!")
+            print(Fore.CYAN + "Great, You win!!" + Style.RESET_ALL)
             break
         else:
-            print("Wrong Hit, Try again!")
+            print(Fore.RED + "Wrong Hit, Try again!" + Style.RESET_ALL)
+
+
     except ValueError:
-        print("Please enter valid numbers.")
+        print(Fore.MAGENTA + "Please enter valid numbers." + Style.RESET_ALL)
