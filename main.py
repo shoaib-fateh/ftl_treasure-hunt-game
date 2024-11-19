08edabc6a7e8f9dd73fb07bdef4f1493d1d700b4
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-19 19:37:15 +0430

    upg the code to look and function better ⭐

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import random
 
-
-rows = int(input("Rows: "))
-cols = int(input("Cols: "))
+rows = int(input("Enter the number of rows for the game board: "))
+cols = int(input("Enter the number of columns for the game board: "))
 
 board = []
+
+
 
 for r in range(rows):
     row = []
 
     for c in range(cols):
         row.append("*")
-    
-    board.append(row)
 
+    board.append(row)
 
 treasure_row = random.randint(0, rows - 1)
 treasure_col = random.randint(0, cols - 1)
@@ -24,16 +24,16 @@ for row in board:
     print(" ".join(row))
 
 
+
 while True:
     try:
-        treasure_row = int(input(f"Enter the row: (0 to {rows -1}) for treasure: "))
-        treasure_col = int(input(f"Enter the col: (0 to {cols -1}) for treasure: "))
+        guess_row = int(input(f"Choose a row to hide the treasure (0 to {rows - 1}): "))
+        guess_col = int(input(f"Choose a column to hide the treasure {cols - 1}): "))
 
-        if 0 <= treasure_row < rows and 0 <= treasure_col < cols:
-            print("Greate, You win!!")
+        if guess_row == treasure_row and guess_col == treasure_col:
+            print("Great, You win!!")
             break
         else:
             print("Wrong Hit, Try again!")
-
     except ValueError:
-        print("Enter valid numbers.")
+        print("Please enter valid numbers.")
